65327a3427aa26737b394cf86c6de956c272a662
Aleezah Ikram <64039135+Aleezah1429@users.noreply.github.com>
2022-01-15 21:56:46 +0500

    Update card.js

--- src/components/card.js
+++ src/components/card.js
@@ -1,17 +1,56 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "../App.css"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Button, CardGroup, Card, Container, Row } from 'react-bootstrap';
 import test2 from "../assets/img/test_2.jpg";
-import readme from "..//assets/img/readme_logo_icon.png"
-
+import readme from "..//assets/img/readme_logo_icon.png";
 import { FaRegHeart } from "react-icons/fa";
+import fire from "../firebase";
+
 
 function MyCard({ heading }) {
+
+  // Store collection of book details in firestore
+  const ref = fire.firestore().collection("Books").doc("Book_1")
+
+  const [data, setData] = useState([]);
+  const [loader, setLoader] = useState(true);
+
+  function getData() {
+    ref.onSnapshot((querySnapshot) => {
+      const items = []
+      fire.firestore().collection("Books")
+        .get()
+        .then((querySnapshot) => {
+          if (!querySnapshot.empty) {
+            querySnapshot.forEach((doc) => {
+              console.log(doc.id, "=>", doc.data());
+              items.push(doc.data())
+            });
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+      setData(items)
+      setLoader(false)
+    })
+  }
+
+  useEffect(() => {
+    getData();
+    console.log(data)
+  }, [])
+
   return (
     <Container>
-      <Row className="card_greeting">{heading}</Row>
-    
+      <Row className="card_greeting">{heading} </Row>
+      {loader === false && (data.map((book) => (
+        <div key={book.id}>
+          <h1>Book Name: {book.BookName}</h1>
+          <h1>Author Name: {book.AuthorName}</h1>
+        </div>
+      )))}
       <CardGroup className="card_container" >
         <Card className="my_card">
           <Card.Img variant="top" src={test2} className="card_image" />
@@ -25,7 +64,6 @@ function MyCard({ heading }) {
           </Card.Body>
           <Card.Footer className="card_footer">
             <small className="text-muted my_card_text ">Author Name <span className="card_heart">&hearts;</span></small>
-
           </Card.Footer>
         </Card>
         <Card className="my_card">
@@ -90,7 +128,7 @@ function MyCard({ heading }) {
         </Card>
       </CardGroup>
 
-  
+
 
     </Container>
   );
