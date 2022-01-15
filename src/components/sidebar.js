19a65ad3e7afa6dca370d9c059d9edc9cb2920b3
Tashanam Shahbaz <64039299+Tashanam-Shahbaz@users.noreply.github.com>
2022-01-15 23:31:42 +0500

    Can Navigate from one component to another.

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,22 +1,11 @@
 import React, { useState } from "react";
-import '../App.css';
 
+//Router
+import { BrowserRouter, Route, Routes,Link} from "react-router-dom";
 
-// import React, { useEffect, useState } from "react";
-import styled from "styled-components";
-import { AccountBox } from "./accountBox";
-import background from "../assets/img/bg.jpg";
-
-import 'bootstrap/dist/css/bootstrap.min.css';
 //react pro sidebar components
-import {
-    ProSidebar,
-    Menu,
-    MenuItem,
-    SidebarHeader,
-    SidebarFooter,
-    SidebarContent,
-} from "react-pro-sidebar";
+import { ProSidebar, Menu, MenuItem, SidebarHeader, SidebarFooter, SidebarContent, } from "react-pro-sidebar";
+
 //icons from react icons
 import { FaList, FaRegHeart, FaSearch } from "react-icons/fa";
 import { FiHome, FiLogOut, FiArrowLeftCircle, FiArrowRightCircle, FiUser } from "react-icons/fi";
@@ -24,25 +13,26 @@ import { RiPencilLine } from "react-icons/ri";
 import { BiCog } from "react-icons/bi";
 import { SiApacheairflow } from "react-icons/si";
 import { GiAbstract050 } from "react-icons/gi";
+
 //sidebar css from react-pro-sidebar module
 import "react-pro-sidebar/dist/css/styles.css";
 
-
-import GenreCard from "./genre_card";
-import MyCard from './card';
+//components
+import Home from "./home";
 import Search from "./search";
 import Profile from "./profile";
-import Edit_Profile from "./edit_profile";
+import styled from "styled-components";
+import { AccountBox } from "./accountBox";
+import background from "../assets/img/bg.jpg";
+// import LoginForm from "./accountBox/loginForm";
+
 
 function Sidenav() {
-
     const [myHeading, setmyHeading] = useState("");
     const [myMargin, setMargin] = useState("my_layout_2");
     const [data, setdata] = useState(false)
-
     //menuCollapse state using useState hook
     const [menuCollapse, setMenuCollapse] = useState(false)
-
     //custom function that will change menucollapse state from false to true and true to false
     const menuIconClick = () => {
         //condition checking to change state from true to false and vice versa
@@ -53,66 +43,59 @@ function Sidenav() {
 
 
     };
-
-    //     const AppContainer = styled.div`
-    //   width: 100%;
-    //   height: 100%;
-    //   display: flex;
-    //   flex-direction: column;
-    //   align-items: center;
-    //   justify-content: center;
-    // `;
     return (
-        <div >
-            <div id="header">
-                {/* collapsed props to change menu size using menucollapse state */}
-                <ProSidebar collapsed={menuCollapse}>
-                    <SidebarHeader>
-                        <div className="logotext">
-                            {/* Icon change using menucollapse state */}
-                            <p id="myheading">{menuCollapse ? <GiAbstract050 /> : <SiApacheairflow />}{myHeading}</p>
+        <BrowserRouter>
+            <div className="main_sidebar">
+                <div id="header">
+                    {/* collapsed props to change menu size using menucollapse state */}
+                    <ProSidebar collapsed={menuCollapse}>
+                        <SidebarHeader>
+                            <div className="logotext">
+                                {/* Icon change using menucollapse state */}
+                                <p id="myheading">{menuCollapse ? <GiAbstract050 /> : <SiApacheairflow />}{myHeading}</p>
+                            </div>
+                            <div className="closemenu" onClick={() => {
+                                menuIconClick();
+
+                            }}>
+                                {/* changing menu collapse icon on click */}
+                                {menuCollapse ? (
+                                    <FiArrowRightCircle />
+                                ) : (
+                                    <FiArrowLeftCircle />
+                                )}
+                            </div>
+                        </SidebarHeader>
+                        <SidebarContent>
+                            <Menu iconShape="square">
+                                <MenuItem active={true} icon={<FiHome />}><Link to="/home">Home</Link></MenuItem>
+                                <MenuItem icon={<FiUser />}><Link to="/profile">Profile</Link></MenuItem>
+                                <MenuItem icon={<FaSearch />}><Link to="/search">Search</Link></MenuItem>
+                                <MenuItem icon={<FaRegHeart />}><Link to="/favourite">Favourite</Link></MenuItem>
+                                <MenuItem icon={<BiCog />}><Link to="/setting">Settings</Link></MenuItem>
+                            </Menu>
+                        </SidebarContent>
+                        <SidebarFooter>
+                            <Menu iconShape="square">
+                                <MenuItem icon={<FiLogOut />}>Logout</MenuItem>
+                            </Menu>
+                        </SidebarFooter>
+                    </ProSidebar>
+                </div>
+                <div className={myMargin}>
+
+                    <Routes>
+                        <Route path="/home" element={<Home/>}/>
+                        <Route path="/profile" element={<Profile/>} />
+                        <Route path="/search" element={<Search/>} />
+                        <Route path="/setting" element={<AccountBox className = "AuthContainer" />} />
+                      
+                    </Routes>
 
 
-                        </div>
-                        <div className="closemenu" onClick={() => {
-                            menuIconClick();
-                            // sidenav_to_app(menuCollapse);
-                        }}>
-                            {/* changing menu collapse icon on click */}
-                            {menuCollapse ? (
-                                <FiArrowRightCircle />
-                            ) : (
-                                <FiArrowLeftCircle />
-                            )}
-                        </div>
-                    </SidebarHeader>
-                    <SidebarContent>
-                        <Menu iconShape="square">
-                            <MenuItem active={true} icon={<FiHome />}>Home</MenuItem>
-                            <MenuItem icon={<FiUser />}>Profile</MenuItem>
-                            <MenuItem icon={<FaSearch />}>Search</MenuItem>
-                            <MenuItem icon={<FaRegHeart />}>Favourite</MenuItem>
-                            <MenuItem icon={<BiCog />}>Settings</MenuItem>
-                        </Menu>
-                    </SidebarContent>
-                    <SidebarFooter>
-                        <Menu iconShape="square">
-                            <MenuItem icon={<FiLogOut />}>Logout</MenuItem>
-                        </Menu>
-                    </SidebarFooter>
-                </ProSidebar>
+                </div>
             </div>
-            <div className={myMargin}>
-                <GenreCard />
-                <MyCard heading={"Specially made for Tashanam"} />
-                <MyCard heading={"Yours Favourite"} />
-                <Search />
-                <Profile />
-                {/* <Edit_Profile/> */}
-                <AccountBox className = "AuthContainer" />
-            </div>
-
-        </div>
+        </BrowserRouter>
     );
 }
 export default Sidenav;
