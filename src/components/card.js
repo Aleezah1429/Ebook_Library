142e611bc2d90385ee401eedbe31b9a7420a1d20
Aleezah Ikram <64039135+Aleezah1429@users.noreply.github.com>
2022-01-17 19:57:34 +0500

    Update card.js

--- src/components/card.js
+++ src/components/card.js
@@ -2,11 +2,9 @@ import React, { useState, useEffect } from "react";
 import "../App.css"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Button, CardGroup, Card, Container, Row } from 'react-bootstrap';
-import test2 from "../assets/img/test_2.jpg";
-import readme from "..//assets/img/readme_logo_icon.png";
 import { FaRegHeart } from "react-icons/fa";
 import fire from "../firebase";
-
+import readme from "..//assets/img/readme_logo_icon.png";
 
 function MyCard({ heading }) {
 
@@ -16,6 +14,12 @@ function MyCard({ heading }) {
   const [data, setData] = useState([]);
   const [loader, setLoader] = useState(true);
 
+  // States of book img and pdf show
+  const [image, setImage] = useState('');
+  const [UrlImg, setUrlImg] = useState('');
+
+
+  // Get Data of Books From Database
   function getData() {
     ref.onSnapshot((querySnapshot) => {
       const items = []
@@ -39,99 +43,71 @@ function MyCard({ heading }) {
 
   useEffect(() => {
     getData();
+    BookShow();
     console.log(data)
   }, [])
+
+
+  // Book upload
+  function BookShow() {
+
+    // Save url or download link
+    // const upload = () => {
+    //   if (image == null)
+    //     return;
+    //   setUrlImg("Getting Download Link...")
+
+      // Sending File to Firebase Storage
+      // storage.ref(`/images/${image.name}`).put(image)
+      // .on("state_changed", alert("success"), alert, () => {
+
+
+      // // Getting Link of Image
+      fire.storage().ref("Book_1").child("GFX_Mentor.jpg").getDownloadURL()
+        .then((url) => {
+          setUrlImg(url);
+          console.log(url)
+        })
+
+      // Getting Link of Pdf
+      // fire.storage().ref("Book_1").child("Aliza Ikram Resume.pdf").getDownloadURL()
+      //   .then((url) => {
+      //     setUrl(url);
+      //     console.log(url)
+      //   })
+
+
+      // });
+
+
+    // }
+  }
 
   return (
     <Container>
       <Row className="card_greeting">{heading} </Row>
-      {loader === false && (data.map((book) => (
-        <div key={book.id}>
-          <h1>Book Name: {book.BookName}</h1>
-          <h1>Author Name: {book.AuthorName}</h1>
-        </div>
-      )))}
+
       <CardGroup className="card_container" >
-        <Card className="my_card">
-          <Card.Img variant="top" src={test2} className="card_image" />
-          <Card.Img variant="top" src={readme} className="card_image_2" />
-          <Card.Body>
-            <Card.Title className="my_card_title">Book Name</Card.Title>
-            <Card.Text className="my_card_text">
-              This is a Book Intro with supporting text below as a natural lead-in to
-              additional content.
-            </Card.Text>
-          </Card.Body>
-          <Card.Footer className="card_footer">
-            <small className="text-muted my_card_text ">Author Name <span className="card_heart">&hearts;</span></small>
-          </Card.Footer>
-        </Card>
-        <Card className="my_card">
-          <Card.Img variant="top" src={test2} className="card_image" />
-          <Card.Img variant="top" src={readme} className="card_image_2" />
-          <Card.Body>
-            <Card.Title className="my_card_title">Book Name</Card.Title>
-            <Card.Text className="my_card_text">
-              This is a Book Intro with supporting text below as a natural lead-in to
-              additional content.
-            </Card.Text>
-          </Card.Body>
-          <Card.Footer className="card_footer">
-            <small className="text-muted my_card_text ">Author Name <span className="card_heart">&hearts;</span></small>
-
-          </Card.Footer>
-        </Card>
-        <Card className="my_card">
-          <Card.Img variant="top" src={test2} className="card_image" />
-          <Card.Img variant="top" src={readme} className="card_image_2" />
-          <Card.Body>
-            <Card.Title className="my_card_title">Book Name</Card.Title>
-            <Card.Text className="my_card_text">
-              This is a Book Intro with supporting text below as a natural lead-in to
-              additional content.
-            </Card.Text>
-          </Card.Body>
-          <Card.Footer className="card_footer">
-            <small className="text-muted my_card_text ">Author Name <span className="card_heart">&hearts;</span></small>
-
-          </Card.Footer>
-        </Card>
-        <Card className="my_card">
-          <Card.Img variant="top" src={test2} className="card_image" />
-          <Card.Img variant="top" src={readme} className="card_image_2" />
-          <Card.Body>
-            <Card.Title className="my_card_title">Book Name</Card.Title>
-            <Card.Text className="my_card_text">
-              This is a Book Intro with supporting text below as a natural lead-in to
-              additional content.
-            </Card.Text>
-          </Card.Body>
-          <Card.Footer className="card_footer">
-            <small className="text-muted my_card_text ">Author Name <span className="card_heart">&hearts;</span></small>
-
-          </Card.Footer>
-        </Card>
-        <Card className="my_card">
-          <Card.Img variant="top" src={test2} className="card_image" />
-          <Card.Img variant="top" src={readme} className="card_image_2" />
-          <Card.Body>
-            <Card.Title className="my_card_title">Book Name</Card.Title>
-            <Card.Text className="my_card_text">
-              This is a Book Intro with supporting text below as a natural lead-in to
-              additional content.
-            </Card.Text>
-          </Card.Body>
-          <Card.Footer className="card_footer">
-            <small className="text-muted my_card_text ">Author Name <span className="card_heart">&hearts;</span></small>
-
-          </Card.Footer>
-        </Card>
+        {loader === false && (data.map((book) => (
+          <Card className="my_card" key={book.id}>
+            <Card.Img variant="top" src={UrlImg} className="card_image" />
+            <Card.Img variant="top" src={readme} className="card_image_2" />
+            <Card.Body>
+              <Card.Title className="my_card_title">{book.BookName}</Card.Title>
+              <Card.Text className="my_card_text">
+                {book.Description}
+              </Card.Text>
+            </Card.Body>
+            <Card.Footer className="card_footer">
+              <small className="text-muted my_card_text ">{book.AuthorName} <span className="card_heart">&hearts;</span></small>
+            </Card.Footer>
+          </Card>
+        )))}
       </CardGroup>
-
-
 
     </Container>
   );
 }
+
 
 export default MyCard;
