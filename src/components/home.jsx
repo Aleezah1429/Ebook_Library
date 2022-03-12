c3a43ad2b01e87ffd33b2ec9f3bb89fa0e848c2c
Aleezah Ikram <64039135+Aleezah1429@users.noreply.github.com>
2022-03-12 19:27:31 +0500

    Update home.jsx

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,15 +1,12 @@
-import React, { useState, useEffect } from "react";
-import fire from "../firebase";
+import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Button, CardGroup, Card, Row, Col, Container } from "react-bootstrap";
+import { Card, Row, Col, Container } from "react-bootstrap";
 import test2 from "../assets/img/test_2.jpg";
 import "../App.css";
-import { FaRegHeart } from "react-icons/fa";
 import MyCard from "./card";
 
 function Home() {
   var getLemail = localStorage.getItem("Lemail");
-  console.log("SDBSKDSKDSDLK", getLemail);
   var userIdList = getLemail.split("@");
   const userId = userIdList[0];
   const lst = ["Action", "Fantacy", "Mystery", "Comedy", "Teenagers", "Science Fiction"]
@@ -21,6 +18,7 @@ function Home() {
         {Array.from({ length: lst.length }).map((_, idx) => (
           <Col>
             <Card className="my_genre_card my_gere_card_2">
+
                 {/* genre_card_container */}
                 <Row>
                     <Col ><Card.Img variant="top" src={test2} className="genre_card_image" /></Col>
