6e7284fcffd1eb2d6b33ae929659cfb817e9d72b
Aleezah Ikram <64039135+Aleezah1429@users.noreply.github.com>
2022-03-13 00:08:54 +0500

    Update search.js

--- src/components/search.js
+++ src/components/search.js
@@ -2,16 +2,22 @@ import React, { useState, useEffect } from "react";
 import "../App.css"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Button, CardGroup, Card, Container, Row, Col, Modal } from 'react-bootstrap';
-import GenreCard from "./genre_card";
-import MyCard from "./card";
 import fire from "../firebase";
 import readme from "..//assets/img/readme_logo_icon.png";
 import { FaSearch } from "react-icons/fa";
 
-function Search({ heading }) {
-  // States of book img and pdf show
-  const [image, setImage] = useState('');
-  const [UrlImg, setUrlImg] = useState('');
+function Search() {
+
+  var getLemail = localStorage.getItem("Lemail")
+  var userId = getLemail.split("@")
+
+  // const [BookId, setBookId] = useState("");
+
+  const [isFavourite, setisFavourite] = useState("heartcolor_1");
+
+  // Store collection of book details in firestore
+  const ref = fire.firestore().collection("Books")
+
 
   // States for MODAL
   const [modalShow, setModalShow] = useState(false);
@@ -19,7 +25,6 @@ function Search({ heading }) {
 
   // States for Book Data
   const [data, setData] = useState([]);
-  const [loader, setLoader] = useState(true);
 
   // States for Search
   const [value, setValue] = useState("");
@@ -32,67 +37,96 @@ function Search({ heading }) {
     return snapshot.docs.map(doc => doc.data());
   }
 
-function SearchForBook(e){
-  setValue(e.target.value)
-  if (value.length > 0) {
-    getMarker()
-      .then(
-        responseData => {
-          setResult([]);
-          let searchQuery = value.toLowerCase();
-          for (const key in responseData) {
-            let book = responseData[key].BookName.toLowerCase();
-            if (book.slice(0, searchQuery.length).indexOf(searchQuery) !== -1) {
-              console.log("SEARCH QUERY", responseData[0].BookName)
-              console.log("book", responseData[key])
-              setSearchResult(responseData[key])
-              setResult(prevResult => {
-                return [...prevResult, responseData[key].BookName]
-              });
+  function SearchForBook(e) {
+    setValue(e.target.value)
+    if (value.length > 0) {
+      getMarker()
+        .then(
+          responseData => {
+            setResult([]);
+            let searchQuery = value.toLowerCase();
+            for (const key in responseData) {
+              let book = responseData[key].BookName.toLowerCase();
+              if (book.slice(0, searchQuery.length).indexOf(searchQuery) !== -1) {
+                console.log("SEARCH QUERY", responseData[0].BookName)
+                console.log("book", responseData[key])
+                setSearchResult(responseData[key])
+                setResult(prevResult => {
+                  return [...prevResult, responseData[key].BookName]
+                });
+              }
             }
           }
-        }
-      )
-      .catch(error => {
-        console.error();
-      });
-  }
-  else {
-    setResult([])
-  }
+        )
+        .catch(error => {
+          console.error();
+        });
+    }
+    else {
+      setResult([])
+    }
 
 
-}
+  }
 
   useEffect(() => {
-    // if (value.length > 0) {
-    //   getMarker()
-    //     .then(
-    //       responseData => {
-    //         setResult([]);
-    //         let searchQuery = value.toLowerCase();
-    //         for (const key in responseData) {
-    //           let book = responseData[key].BookName.toLowerCase();
-    //           if (book.slice(0, searchQuery.length).indexOf(searchQuery) !== -1) {
-    //             console.log("SEARCH QUERY", responseData[0].BookName)
-    //             console.log("book", responseData[key])
-    //             setSearchResult(responseData[key])
-    //             setResult(prevResult => {
-    //               return [...prevResult, responseData[key].BookName]
-    //             });
-    //           }
-    //         }
-    //       }
-    //     )
-    //     .catch(error => {
-    //       console.error();
-    //     });
-    // }
-    // else {
-    //   setResult([])
-    // }
+    ref.onSnapshot((querySnapshot) => {
+      const items = [];
+      fire
+        .firestore()
+        .collection("Books")
+        .get()
+        .then((querySnapshot) => {
+          if (!querySnapshot.empty) {
+            querySnapshot.forEach((doc) => {
+              items.push(doc.data());
+            });
+          }
+          setData(items);
+        })
 
+        .catch((error) => {
+          console.log(error);
+        });
+    });
+    console.log(data);
   }, [value])
+
+
+  // Add to favourite
+  const AddToFavourites = (id, name) => {
+    let arr;
+    fire.firestore().collection("Favourite_Books").doc(userId[0]).get().then((snap) => {
+      if (snap.data() == undefined) {
+        arr = []
+      }
+      else {
+        arr = snap.data().Books
+      }
+
+      const singleObj = { id, name }
+      arr.push(JSON.stringify(singleObj))
+      console.log("name", name)
+
+      // Set on Firestore
+      fire
+        .firestore()
+        .collection("Favourite_Books")
+        .doc(userId[0])
+        .set(
+          {
+            Books: arr
+          }).then(
+            alert(`${name} Added to Favourite`)
+          )
+
+    })
+  }
+
+
+  function OpenPdf(id, pdf) {
+    window.open(pdf, '_blank');
+  }
 
   return (
     <Container className="search_bar">
@@ -104,17 +138,14 @@ function SearchForBook(e){
         <button type="submit"><FaSearch /></button>
       </Container>
       <div>
-        {/* <h6 className="searchList">{result}</h6> */}
       </div>
       <Container>
-        <Row className="card_greeting">{heading}</Row>
         <Row xs={1} sm={3} md={5} className="g-4">
-          {/* {console.log("RESULTTT",SearchResult)} */}
-          
-            <Col>
-            {(SearchResult)?
+
+          <Col>
+            {(SearchResult) ?
               <Card className="my_card" key={SearchResult.id}>
-                <Card.Img variant="top" src={SearchResult.Image} className="card_image" />
+                <Card.Img variant="top" src={SearchResult.Image} className="card_image" onClick={() => OpenPdf(SearchResult.id, SearchResult.Pdf)} />
                 <Card.Img variant="top" src={readme} className="card_image_2" />
 
                 <Card.Body>
@@ -126,11 +157,12 @@ function SearchForBook(e){
                 <Card.Footer className="card_footer">
                   <small className="text-muted my_card_text ">
                     {SearchResult.AuthorName}
-                    <span type="button"
-                      // heart color will be ornge if it is present in database otherwise grey
-                      className={`${SearchResult.favorite ? "heartcolor_1" : "heartcolor_2"}`}
-                    // Aleezah apply condition that if its click favourte will asssigne oppsite boolen values
-                    // onClick={}
+                    <span
+                      type="button"
+                      onClick={() => {
+                        AddToFavourites(SearchResult.id, SearchResult.BookName)
+                      }}
+                      className={isFavourite}
                     >
                       &hearts;
                     </span>
@@ -170,9 +202,9 @@ function SearchForBook(e){
                   </Modal.Footer>
                 </Modal>
               </Card>
-              :null}
-            </Col>
-          
+              : null}
+          </Col>
+
         </Row>
       </Container>
 
